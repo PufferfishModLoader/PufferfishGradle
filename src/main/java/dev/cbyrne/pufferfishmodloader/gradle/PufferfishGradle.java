cde7b21d74200a2917890c6b3ec40c9944fa14ec
AmpTheDev <gerritegberink3@gmail.com>
2020-04-23 19:55:54 +0200

    Add basic plugin implementation

---
+++ src/main/java/dev/cbyrne/pufferfishmodloader/gradle/PufferfishGradle.java
@@ -0,0 +1,13 @@
+package dev.cbyrne.pufferfishmodloader.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import javax.annotation.Nonnull;
+
+public class PufferfishGradle implements Plugin<Project> {
+    @Override
+    public void apply(@Nonnull Project project) {
+
+    }
+}
